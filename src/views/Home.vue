07b9ae70c11b614e2829dfa316b704ce7bf0d379
Calden Rodrigues <caldenrodrigues1202@gmail.com>
2019-02-21 19:32:15 +0530

    Finished functionality of inventory, Items can now be added and data will be updated dynamically

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>Inventory</h1>
+    <InventoryTable />
+  </div>
+</template>
+
+<script>
+import InventoryTable from '../components/InventoryTable'
+
+export default {
+  name: 'Home',
+  components: {
+    InventoryTable
+  }
+}
+</script>
