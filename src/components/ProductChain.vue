d8c571f584ab6b21014e7ab4eb84cbab46927d91
Calden Rodrigues <caldenrodrigues1202@gmail.com>
2019-02-25 12:03:05 +0530

    updated data in chain

--- src/components/ProductChain.vue
+++ src/components/ProductChain.vue
@@ -17,12 +17,15 @@
             >
               mdi-magnify
             </v-icon-->
-            <h2 class="display-1 white--text font-weight-light">Supplier</h2>
+            <h2 class="display-1 white--text font-weight-light">Raw Material Supplier</h2>
           </v-card-title>
           <v-container>
             <v-layout>
               <v-flex xs10>
-                Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit.
+                <pre>
+                  Organization Name : Farmer Rocks
+                  Address : Some Random Address in nagpur.
+                </pre>
               </v-flex>
               <v-flex xs2>
                 <v-icon size="64">mdi-calendar-text</v-icon>
@@ -79,7 +82,12 @@
             <v-card-title class="headline">Sales Order</v-card-title>
 
             <v-card-text>
-              Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.
+              <pre>
+Payment ID: 1001234
+Material Name: Coffee beans
+Quantity: 10kg
+Amount: 10500
+              </pre>
             </v-card-text>
 
             <v-card-actions>
@@ -96,70 +104,107 @@
           </v-card>
         </v-dialog>
 
-          <v-timeline-item
-            class="mb-3"
-            small
-          >
-            <v-layout justify-space-between>
-              <v-flex xs7>
-                <v-chip
-                  class="white--text ml-0"
-                  color="purple"
-                  label
-                  small
-                >
-                  APP
-                </v-chip>
-                Digital Downloads fulfilled 1 item.
-              </v-flex>
-              <v-flex xs5 text-xs-right>15:25 EDT</v-flex>
-            </v-layout>
-          </v-timeline-item>
 
-          <v-timeline-item
-            class="mb-3"
-            color="grey"
-            small
-          >
-            <v-layout justify-space-between>
-              <v-flex xs7>
-                Order confirmation email was sent to John Leider (john@vuetifyjs.com).
-              </v-flex>
-              <v-flex xs5 text-xs-right>15:25 EDT</v-flex>
-            </v-layout>
-          </v-timeline-item>
+              <v-timeline-item
+                class="mb-3"
+                icon-color="grey lighten-2"
+                small
+              >
+                <v-layout justify-space-between>
+                  <v-flex xs7>
+                    <v-btn small color="primary" dark @click="packTicket = true">Packing Ticket</v-btn>
+                    Order is packed.
+                  </v-flex>
+                  <v-flex xs5 text-xs-right>18:45 IST</v-flex>
+                </v-layout>
+              </v-timeline-item>
 
-          <v-timeline-item
-            class="mb-3"
-            hide-dot
-          >
-            <v-btn
-              class="mx-0"
-              color="white"
+              <!-- Dialog for Sales Order-->
+              <v-dialog
+              v-model="packTicket"
+              max-width="290"
             >
-              Resend Email
-            </v-btn>
-          </v-timeline-item>
+              <v-card>
+                <v-card-title class="headline">Packing Ticket</v-card-title>
 
-          <v-timeline-item
-            class="mb-3"
-            color="grey"
-            small
+                <v-card-text>
+                  <pre>
+    Packment ID: 12345
+    Material Name: Coffee beans
+    Quantity: 10kg
+    Delivery Date: 12/02/1999
+                  </pre>
+                </v-card-text>
+
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+
+                  <v-btn
+                    color="green darken-1"
+                    flat="flat"
+                    @click="packTicket = false"
+                  >
+                    OK
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+
+            <v-timeline-item
+              class="mb-3"
+              icon-color="grey lighten-2"
+              small
+            >
+              <v-layout justify-space-between>
+                <v-flex xs7>
+                  <v-btn small color="primary" dark @click="shipTicket = true">Shipment Ticket</v-btn>
+                  Order has arrived.
+                </v-flex>
+                <v-flex xs5 text-xs-right>22:45 IST</v-flex>
+              </v-layout>
+            </v-timeline-item>
+
+            <!-- Dialog for Sales Order-->
+            <v-dialog
+            v-model="shipTicket"
+            max-width="290"
           >
-            <v-layout justify-space-between>
-              <v-flex xs7>
-                A $15.00 USD payment was processed on PayPal Express Checkout
-              </v-flex>
-              <v-flex xs5 text-xs-right>15:25 EDT</v-flex>
-            </v-layout>
-          </v-timeline-item>
+            <v-card>
+              <v-card-title class="headline">Shipment Ticket</v-card-title>
 
-          <v-timeline-item
-        color="blue accent-4"
-        fill-dot
-        right
-      >
-        <v-card>
+              <v-card-text>
+                <pre>
+  Shipment ID: 12345
+  Material Name: Coffee beans
+  Quantity: 10kg
+  Holes: false
+  Condensation: false
+  Insect Activity: false
+                </pre>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+
+                <v-btn
+                  color="green darken-1"
+                  flat="flat"
+                  @click="shipTicket = false"
+                >
+                  OK
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+
+
+
+            <v-timeline-item
+          color="blue accent-4"
+          fill-dot
+          right
+        >
+          <v-card>
             <v-card-title class="blue accent-4">
             <v-icon
               dark
@@ -168,12 +213,15 @@
             >
               mdi-magnify
             </v-icon>
-            <h2 class="display-1 white--text font-weight-light">Manufacturer</h2>
+            <h2 class="display-1 white--text font-weight-light">Trader</h2>
           </v-card-title>
           <v-container>
             <v-layout>
               <v-flex xs10>
-                Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit.
+                <pre>
+                  Organization Name : Trader Rocks
+                  Address : Some Trader Address in nagpur.
+                </pre>
               </v-flex>
               <v-flex xs2>
                 <v-icon size="64">mdi-calendar-text</v-icon>
@@ -195,6 +243,8 @@ export default {
   data() {
     return {
       salesDialog: false,
+      packTicket: false,
+      shipTicket: false
     }
   },
 }
