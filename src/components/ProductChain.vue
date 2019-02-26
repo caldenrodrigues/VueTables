509ff3d2bbd67486b4c2654fa4ad617a14e295bc
Calden Rodrigues <caldenrodrigues1202@gmail.com>
2019-02-26 12:52:46 +0530

    Updated Css, and taking data from JSON

--- src/components/ProductChain.vue
+++ src/components/ProductChain.vue
@@ -22,9 +22,9 @@
           <v-container>
             <v-layout>
               <v-flex xs10>
-                  <h2 class="text-lg-left blue--text text--lighten-1">Organization Name : </h2><h3 class="text-lg-left"> Trader Rocks</h3>
+                  <h2 class="text-lg-left blue--text text--lighten-1">Organization Name : </h2><h3 class="text-lg-left">{{chainDetails.rawMaterialSupplier.name}}</h3>
                   <br />
-                  <h2 class="text-lg-left blue--text text--lighten-1">Address :</h2><h3 class="text-lg-left"> Some Trader Address in nagpur. <br />Maharastra<br />India</h3>
+                  <h2 class="text-lg-left blue--text text--lighten-1">Address :</h2><h3 class="text-lg-left"> {{chainDetails.rawMaterialSupplier.address1}} <br />{{chainDetails.rawMaterialSupplier.address2}}<br />{{chainDetails.rawMaterialSupplier.address3}}</h3>
 
               </v-flex>
               <v-flex xs2>
@@ -42,24 +42,14 @@
       >
         <v-layout justify-space-between>
           <v-flex xs7>
-            The Order was placed by "Awesome Trader"
+            The Order was placed by "Awesome Trader"<br/><br/>
+            <v-btn small color="primary" dark @click="salesDialog = true">Sales Order</v-btn>
           </v-flex>
           <v-flex xs5 text-xs-right>15:26 EDT</v-flex>
         </v-layout>
       </v-timeline-item>
 
-      <v-timeline-item
-        class="mb-3"
-        icon-color="grey lighten-2"
-        small
-        hide-dot
-      >
-        <v-layout justify-space-between>
-          <v-flex xs7>
-            <v-btn small color="primary" dark @click="salesDialog = true">Sales Order</v-btn>
-          </v-flex>
-        </v-layout>
-      </v-timeline-item>
+
 
           <!-- Dialog for Sales Order-->
       <v-dialog
@@ -98,22 +88,10 @@
       >
         <v-layout justify-space-between>
           <v-flex xs7>
-            Order is packed and ready to be shipped.
-          </v-flex>
-          <v-flex xs5 text-xs-right>18:45 IST</v-flex>
-        </v-layout>
-      </v-timeline-item>
-
-      <v-timeline-item
-        class="mb-3"
-        icon-color="grey lighten-2"
-        small
-        hide-dot
-      >
-        <v-layout justify-space-between>
-          <v-flex xs7>
+            Order is packed and ready to be shipped.<br/><br/>
             <v-btn small color="primary" dark @click="packTicket = true">Packing Ticket</v-btn>
           </v-flex>
+          <v-flex xs5 text-xs-right>18:45 IST</v-flex>
         </v-layout>
       </v-timeline-item>
 
@@ -122,16 +100,14 @@
         v-model="packTicket"
         max-width="290"
       >
-        <v-card>
-          <v-card-title class="headline">Packing Ticket</v-card-title>
+        <v-card >
+          <v-card-title class="headline  text-lg-left blue--text">Packing Ticket</v-card-title>
 
-          <v-card-text>
-            <pre>
-    Packment ID: 12345
-    Material Name: Coffee beans
-    Quantity: 10kg
-    Delivery Date: 12/02/1999
-            </pre>
+          <v-card-text class="text-lg-left">
+            <h3>Packment ID: 12345<br />
+            Material Name: Coffee beans<br />
+            Quantity: 10kg<br />
+            Delivery Date: 12/02/1999</h3>
           </v-card-text>
 
           <v-card-actions>
@@ -155,24 +131,13 @@
       >
         <v-layout justify-space-between>
           <v-flex xs7>
-            Order has arrived at "Awesome Place".
+            Order has arrived at "Awesome Place".<br /><br />
+            <v-btn small color="primary" dark @click="shipTicket = true">Shipment Ticket</v-btn>
           </v-flex>
           <v-flex xs5 text-xs-right>22:45 IST</v-flex>
         </v-layout>
       </v-timeline-item>
 
-      <v-timeline-item
-        class="mb-3"
-        icon-color="grey lighten-2"
-        small
-        hide-dot
-      >
-        <v-layout justify-space-between>
-          <v-flex xs7>
-            <v-btn small color="primary" dark @click="shipTicket = true">Shipment Ticket</v-btn>
-          </v-flex>
-        </v-layout>
-      </v-timeline-item>
 
             <!-- Dialog for Sales Order-->
       <v-dialog
@@ -180,17 +145,16 @@
         max-width="290"
       >
         <v-card>
-          <v-card-title class="headline">Shipment Ticket</v-card-title>
+          <v-card-title class="headline  text-lg-left blue--text">Shipment Ticket</v-card-title>
 
-          <v-card-text>
-            <pre>
-    Shipment ID: 12345
-    Material Name: Coffee beans
-    Quantity: 10kg
-    Holes: false
-    Condensation: false
-    Insect Activity: false
-            </pre>
+          <v-card-text class="text-lg-left">
+            <h3>Packment ID: 12345<br />
+            Material Name: Coffee beans<br />
+            Quantity: 10kg<br />
+            Holes: false<br />
+            Condensation: false<br />
+            Insect Activity: false<br />
+            Delivery Date: 12/02/1999</h3>
           </v-card-text>
 
           <v-card-actions>
@@ -229,9 +193,9 @@
               <v-layout>
 
                   <v-flex xs10>
-                      <h2 class="text-lg-left blue--text text--lighten-1">Organization Name : </h2><h3 class="text-lg-left"> Trader Rocks</h3>
+                      <h2 class="text-lg-left blue--text text--lighten-1">Organization Name : </h2><h3 class="text-lg-left">{{chainDetails.trader.name}}</h3>
                       <br />
-                      <h2 class="text-lg-left blue--text text--lighten-1">Address :</h2><h3 class="text-lg-left"> Some Trader Address in nagpur. <br>Maharastra<br />India</h3>
+                      <h2 class="text-lg-left blue--text text--lighten-1">Address :</h2><h3 class="text-lg-left">{{chainDetails.trader.address1}}<br>{{chainDetails.trader.address2}}<br />{{chainDetails.trader.address3}}</h3>
                   </v-flex>
 
 
@@ -256,7 +220,21 @@ export default {
     return {
       salesDialog: false,
       packTicket: false,
-      shipTicket: false
+      shipTicket: false,
+      chainDetails: {
+        rawMaterialSupplier: {
+          name: "Supplier Rocks",
+          address1: "Some Supplier Address in nagpur,",
+          address2: "Maharastra",
+          address3: "India"
+        },
+        trader: {
+          name: "Trader Rocks",
+          address1: "Some Trader Address in nagpur,",
+          address2: "Maharastra",
+          address3: "India"
+        }
+      }
     }
   },
 }
